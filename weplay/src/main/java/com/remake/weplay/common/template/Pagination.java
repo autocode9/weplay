ff9934dac9b3d0fde492fa8e740ac4ada2f19049
kimseunghwan123 <jonny235911@naver.com>
2024-05-13 09:23:31 +0900

    update Pagination

--- weplay/src/main/java/com/remake/weplay/common/template/Pagination.java
+++ weplay/src/main/java/com/remake/weplay/common/template/Pagination.java
@@ -7,7 +7,6 @@ import com.remake.weplay.common.model.vo.PageInfo;
 public class Pagination {
 	
 	public static PageInfo getPageInfo(int listCount, int currentPage, int boardLimit, int pageLimit) {
-		
 		int maxPage = (int)Math.ceil((double)listCount / boardLimit);
 		int startPage = (currentPage - 1) / pageLimit * pageLimit + 1;
 		int endPage = startPage + pageLimit - 1;

3d5c535b9b38958aa8a0df28e6948c3dacc4d9e5
kimseunghwan123 <jonny235911@naver.com>
2024-05-17 17:26:26 +0900

    11

--- weplay/src/main/java/com/remake/weplay/team/model/service/TeamService.java
+++ weplay/src/main/java/com/remake/weplay/team/model/service/TeamService.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import org.apache.ibatis.session.RowBounds;
 
-import com.google.gson.JsonElement;
 import com.remake.weplay.team.model.vo.Team;
 import com.remake.weplay.team.model.vo.TeamApplication;
 

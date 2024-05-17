a5eda93321388cedb0108acf6a072bb2f584ff2d
LeeSeongWook1 <tjddnr0761@gmail.com>
2024-05-17 16:23:29 +0900

    13

--- weplay/src/main/java/com/remake/weplay/teamBoard/controller/TeamBoardController.java
+++ weplay/src/main/java/com/remake/weplay/teamBoard/controller/TeamBoardController.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.google.gson.Gson;
 import com.remake.weplay.teamBoard.model.service.TeamBoardService;
+import com.remake.weplay.teamBoard.model.vo.TeamBoard;
 import com.remake.weplay.teamBoardBrg.model.service.TeamBoardBrgService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -30,6 +32,12 @@ public class TeamBoardController {
 		
 		return new Gson().toJson(teamBoardService.selectList(boardCode));
 		
+	}
+	
+	@ResponseBody
+	@PostMapping("teamboardSelect")
+	public String insertBoard(TeamBoard teamBoard) {
+		return teamBoardService.insertTeamBoard(teamBoard)>0 ? "success" : "fail";
 	}
 	
 }

0c7c958f0f317689703882284be6f5ed942addb8
WonjunJung <jun1221909@gmail.com>
2024-05-20 15:16:53 +0900

    11

--- weplay/src/main/java/com/remake/weplay/commons/interceptor/LoginInterceptor.java
+++ weplay/src/main/java/com/remake/weplay/commons/interceptor/LoginInterceptor.java
@@ -7,23 +7,10 @@ import javax.servlet.http.HttpSession;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class LoginInterceptor extends HandlerInterceptorAdapter {
-	
-	/*
-	 * Interceptor(정확히 HandlerInterceptor)
-	 * 
-	 * Handler가 호출되기 전, 실행된 후 가로채서 실행할 내용을 작성 가능
-	 * 
-	 * preHandler(전 처리 : 핸들러 호출 전 낚아챔)
-	 * postHandler(후 처리 : 요청 처리 후 낚아챔)
-	 * 
-	 * 
-	 */
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		// true 반환 => 기존 요청 호출대로 Handler를 정상 수행 ==> Controller에 있는 메소드 호출
-		// false 반환 => Handler호출 안함
 		HttpSession session = request.getSession();
 		if(session.getAttribute("loginUser") != null) {
 			return true;
